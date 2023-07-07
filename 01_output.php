acb2725f14c723836e3d886f596d1fc3794d8ba4
Daniel Burgess <dan-burgess@hotmail.com>
2023-07-07 14:52:10 +0930

    initial commit

---
+++ 01_output.php
@@ -0,0 +1,17 @@
+<?php
+  // echo 'Hellllllo';
+ // print_r([1,2,3]);
+// var_dump(5.3);
+  var_export('hello');
+?>
+
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <h1><?php echo 'post one' ?></h1>
+</body>
+</html>
