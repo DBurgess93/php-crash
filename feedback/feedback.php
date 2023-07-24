0adf960078d9c246df63eb1647c4d298b802c803
Daniel Burgess <dan-burgess@hotmail.com>
2023-07-24 23:25:39 +0930

    Add feedback dynamically (hard coded for now)

--- feedback/feedback.php
+++ feedback/feedback.php
@@ -1,22 +1,42 @@
 <?php include 'inc/header.php' ?>
 
-<h2>Feedback</h2>
+<?php
+$feedback = [
+  [
+    'id' => '1',
+    'name' => 'Tom Rom',
+    'email' => 'tom@email.com',
+    'body' => 'Im a goofy goober yeah'
+  ],
+  [
+    'id' => '2',
+    'name' => 'Flom Rom',
+    'email' => 'flom@email.com',
+    'body' => 'You dont need a licence to drive a sandwich'
+  ],
+  [
+    'id' => '3',
+    'name' => 'Clom Rom',
+    'email' => 'clom@email.com',
+    'body' => 'My eyes! i cant feel my eyes!'
+  ],
+];
+?>
 
-<div class="card my-3">
-  <div class="card-body">
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta molestias animi earum eos dolorem repellat a quibusdam, aperiam vero repellendus voluptatibus natus deserunt sed doloribus inventore, totam labore maxime perferendis!
-  </div>
-</div>
+<h2>Past Feedback</h2>
 
-<div class="card my-3">
-  <div class="card-body">
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta molestias animi earum eos dolorem repellat a quibusdam, aperiam vero repellendus voluptatibus natus deserunt sed doloribus inventore, totam labore maxime perferendis!
-  </div>
-</div>
+<?php if (empty($feedback)) : ?>
+  <p class="lead mt3"> There is no feedback </p>
+<?php endif; ?>
 
-<div class="card my-3">
-  <div class="card-body">
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta molestias animi earum eos dolorem repellat a quibusdam, aperiam vero repellendus voluptatibus natus deserunt sed doloribus inventore, totam labore maxime perferendis!
+<?php foreach ($feedback as $item) : ?>
+  <div class="card my-3 w-75">
+    <div class="card-body text-center">
+      <?php echo $item['body']; ?>
+      <div class="text-secondary mt-2">
+        By <?php echo $item['name']; ?>
+      </div>
+    </div>
   </div>
-</div>
+<?php endforeach ?>
 <?php include 'inc/footer.php' ?>
